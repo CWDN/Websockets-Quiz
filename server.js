1d42e2d0dc11d3042b8088de265431497ab9698e
Piefarmer <chris.normansell@gmail.com>
2014-10-15 23:45:20 +0100

    Started building quiz

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
-var app = require('express')();
+/* region Globals*/
+
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+/* CSS Preprocessor*/
+var stylus = require('stylus');
+/* Stylus helper */
+var nib = require('nib');
+/* File system library */
 var fs = require('fs');
+/* Logger */
+var morgan = require('morgan');
 var io = require('socket.io')(http);
 
+/* endregion */
+
+function compile(str, path){
+	return stylus(str)
+		.set('filename', path)
+		.use(nib());
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: accessLogStream}));
+
+
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
+
+
 app.get('/', function(req, res){
-   res.sendFile(__dirname + '/chat.html');
+   res.render('index',
+			  {
+	   title : 'Home',
+	   team: 'The Oldies',
+	   currentQuestion: '19',
+	   totalQuestions: '60',
+	   category: 'Music',
+	   question: 'What is the name of the offical national anthem of the USA?'
+
+   })
 });
 
+/*
 app.get('/test', function(req, res){
    res.sendFile(__dirname + '/client/test.html');
 });
@@ -49,6 +92,7 @@ io.on('connection', function(socket){
         socket.emit('image', { buffer: buf });
       });
 })
+*/
 
  var server = app.listen(8000, function()
 {
