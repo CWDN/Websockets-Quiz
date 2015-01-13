48bcd8d150243a740c90fb18d5703a3b0ebc1095
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-13 09:37:52 +0000

    Created couple of quiz migrations

--- migrations/seeds/categories.sql
+++ migrations/seeds/categories.sql
@@ -1,7 +1,2 @@
 INSERT INTO `categories` (`id`, `category`) VALUES
-(1, 'Music'),
-(2, 'Geography'),
-(3, 'History'),
-(4, 'Food and Drink'),
-(5, 'Art and Philosophy'),
-(6, 'General Knowledge');
+(1, 'General Knowledge');
